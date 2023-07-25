b3dd6c3d960a84dffbb49ba8a6a3a1f07d9fbbf1
Yikoutian1 <599882460@qq.com>
2023-07-25 16:53:07 +0800

    v-model拆分

---
+++ Day4/12-v-model简化代码/App copy.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <BaseSelect v-model="selectId"></BaseSelect>
+  </div>
+</template>
+
+<script>
+import BaseSelect from "./components/BaseSelect.vue";
+export default {
+  data() {
+    return {
+      selectId: "102",
+    };
+  },
+  components: {
+    BaseSelect,
+  },
+};
+</script>
+
+<style></style>
--- Day4/12-v-model简化代码/components/BaseSelect.vue
+++ Day4/12-v-model简化代码/components/BaseSelect.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <select>
+    <select :value="value" @change="handleChange">
       <option value="101">北京</option>
       <option value="102">上海</option>
       <option value="103">武汉</option>
@@ -12,7 +12,15 @@
 
 <script>
 export default {
-  
+  props: {
+    value: String
+  },
+  methods:{
+    handleChange(e){
+      // 在子组件中,value接收,监听input事件
+      this.$emit('input',e.target.value)
+    }
+  }
 }
 </script>
 
