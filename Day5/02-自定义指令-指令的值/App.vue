7c1f4a5f20606cc18e57a82e4fe05aa07602dddd
Yikoutian1 <599882460@qq.com>
2023-07-30 10:30:20 +0800

    自定义的值，v-自定义

--- Day5/02-自定义指令-指令的值/App.vue
+++ Day5/02-自定义指令-指令的值/App.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
-    <h1>指令的值1测试</h1>
-    <h1>指令的值2测试</h1>
+    <h1 v-color="red">指令的值1测试</h1>
+    <h1 v-color="blue">指令的值2测试</h1>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      
+      red: 'red',
+      blue: 'blue'
+    };
+  },
+  directives: {
+    color:{
+      // 1. inserted 提供的是元素被添加到页面时添加（后续在修改网页的时候不会触发）
+      inserted(el,binding){
+        // binding.value 就是指令的值
+        el.style.color = binding.value
+      },
+      // 2. update 指令的值修改的时候触发，提供值变化后，dom更新的逻辑
+      // 在网页实例（F12切换vue）
+      update(el,binding){
+        el.style.color = binding.value
+      }
     }
-  }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div>
-    <h1>自定义指令</h1>
-    <input v-focus ref="inp" type="text">
+    <h1 v-color="red">指令的值1测试</h1>
+    <h1 v-color="blue">指令的值2测试</h1>
   </div>
 </template>
 
 <script>
 export default {
-  // mounted () {
-  //   this.$refs.inp.focus()
-  // }
-  
-  // 2. 局部注册指令
+  data() {
+    return {
+      red: 'red',
+      blue: 'blue'
+    };
+  },
   directives: {
-    // 指令名：指令的配置项
-    focus: {
-      inserted (el) {
-        el.focus()
+    color:{
+      // 1. inserted 提供的是元素被添加到页面时添加（后续在修改网页的时候不会触发）
+      inserted(el,binding){
+        // binding.value 就是指令的值
+        el.style.color = binding.value
+      },
+      // 2. update 指令的值修改的时候触发，提供值变化后，dom更新的逻辑
+      // 在网页实例（F12切换vue）
+      update(el,binding){
+        el.style.color = binding.value
       }
     }
-  }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
